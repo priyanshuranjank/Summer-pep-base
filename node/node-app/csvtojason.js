2388721475e64dac9523fdfcc6f14fa9c7558fd4
priyanshuranjank <priyanshuranjan1r@gmail.com>
2025-07-13 23:57:55 +0530

    day-16

--- node/node-app/csvtojason.js
+++ node/node-app/csvtojason.js
@@ -1,11 +1,11 @@
 const csv = require('csvtojson');
 const path = require('path');
 
-// const csvFilePath = path.join(__dirname, './data/info.csv');
-// async function parseCSV() {
+const csvFilePath = path.join(__dirname, './data/info.csv');
+async function parseCSV() {
     
-//     const jsonArray = await csv().fromFile(csvFilePath);
-//     console.log(jsonArray);
-// }
-// // export default parseCSV;
-// parseCSV();
+    const jsonArray = await csv().fromFile(csvFilePath);
+    console.log(jsonArray);
+}
+// export default parseCSV;
+parseCSV();
